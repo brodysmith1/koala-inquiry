c07806af399512f7ee45a5c2bfc321c1d64ab29a
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-08-16 18:37:03 +1000

    d3 installed

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,10 +1,18 @@
 // Vars
 var x = 0;
+var n = 4;
 var i = 0;
 
 // Event listeners
+document.addEventListener('DOMContentLoaded', onLoad);   // TODO: DEBOUNCE
 document.addEventListener('keydown', checkKey);   // TODO: DEBOUNCE
 window.onresize = onResize;                       // TODO: DEBOUNCE
+
+
+// Load
+function onLoad() {
+  onSlide(0);
+}
 
 
 // Resize
@@ -18,24 +26,58 @@ function onResize(e) {
 
 // Check keypress
 function checkKey(e) {
-  e.keyCode == 37 ? slide(true) :
-  e.keyCode == 39 ? slide(false) : "";
+  e.keyCode == 39 ? checkX(true) :    // right
+  e.keyCode == 37 ? checkX(false) :   // left
+  e.keyCode == 35 ? jumpTo(n) :       // end
+  e.keyCode == 36 ? jumpTo(0) : "";   // home
 }
 
 
-// Slide
+// Slide across
 function slide(fwd) {
+  let p = i;
   let slides = document.querySelector('.slides');
-  let slide = document.querySelector('.slide');
-  let dx = slide.getBoundingClientRect().width;
+  let dx = document.querySelector('.slide').getBoundingClientRect().width;
 
-  x += fwd ? dx : -dx;
-  i += fwd ? -1 : 1;
+  x += fwd ? -dx : dx;
+  i += fwd ? 1 : -1;
+
   translateX(slides, x);
+  onSlide(p);
+}
+
+
+// Execute methods when a slide becomes in view
+function onSlide(p) {
+  let prev = document.querySelectorAll('.slide')[p];
+  let cur = document.querySelectorAll('.slide')[i];
+  let bgPrev = prev.querySelector('.bg-image');
+  let bgCur = cur.querySelector('.bg-image');
+
+  bgPrev ? bgPrev.classList.remove('active') : "";
+  bgCur  ? bgCur.classList.add('active')  : "";
+
+}
+
+
+// Jump to specific slide
+function jumpTo(index) {
+  let p = i;
+  let slides = document.querySelector('.slides');
+  let vw = document.querySelector('.slide').getBoundingClientRect().width;
+
+  i = index;
+  x = - index * vw;
+  translateX(slides, x);
+  onSlide(p);
 }
 
 
 // Helpers
-function translateX(el, x) {
+function translateX(el, x) {                  // Apply slide transform
   el.style.transform = `translateX(${x}px)`;
+}
+function checkX(fwd) {                        // Check for start or end position
+  if (fwd) { i<n ? slide(fwd) : ""; }
+  else { i==0 ? "" : slide(fwd); }
 }
