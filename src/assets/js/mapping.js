ca8cae6dd98ff405b3cd869475e9cd58981777ac
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-08-21 14:56:29 +1000

    d3 map tinkering

--- src/assets/js/mapping.js
+++ src/assets/js/mapping.js
@@ -1,20 +1,54 @@
 import * as d3 from "d3";
 import * as topojson from "topojson"
 
+
 let nsw = "../map-data/nsw-outline.json";
-console.log(nsw);
 
-let topojsonObject = topojson.feature(nsw, nsw.FeatureCollection.features);
-let topojsonDataSet = topojsonObject.features;
+console.log(d3);
 
-let width = 960,
-    height = 1160;
+// Map defs
+let projection = d3.geoEquirectangular();
+let path = d3.geoPath().projection(projection);
 
-const svg = d3.create("svg").attr("viewBox", [0, 0, 975, 610]);
+// let topology = topojson.topology({foo: json})
+// let topojsonObject = topojson.feature(nsw, nsw.FeatureCollection.features);
+// let topojsonDataSet = topojsonObject.features;
 
-svg.append("g")
-  .selectAll("path")
-  .data(topojsonDataSet)
-  .join("path")
-  .attr("fill", d => color(data.get(d.id)))
-  .attr("d", path);
+let width = 1200,
+    height = 800;
+
+var svg = d3.select("body #nsw-map")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height])
+    .attr("preserveAspectRatio", "xMinYMin");
+
+
+// svg.append("g")
+//   .selectAll("path")
+//   .data(topojsonDataSet)
+//   .join("path")
+//   .attr("fill", d => color(data.get(d.id)))
+//   .attr("d", path);
+
+// d3.json(nsw, function(error, mapData) {
+//   var features = mapData.features;
+//
+//   // Update color scale domain based on data
+//   color.domain([0, d3.max(features, nameLength)]);
+//
+//   // Draw each province as a path
+//   mapLayer.selectAll('path')
+//       .data(features)
+//       .enter()
+//       .append('path')
+//       .attr('d', path)
+//       .attr('vector-effect', 'non-scaling-stroke')
+//       .style('fill', fillFn)
+//       .on('mouseover', mouseover)
+//       .on('mouseout', mouseout)
+//       .on('click', clicked);
+// });
+
+console.log(svg);
