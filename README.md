737adca5475aa778bfba85018198355b95d4194a
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-08-23 20:23:21 +1000

    Mapp added

--- README.md
+++ README.md
@@ -3,11 +3,15 @@
 ## Data sources
 - NSW Suburbs Base Map - https://data.gov.au/dataset/ds-dga-91e70237-d9d1-4719-a82f-e71b811154c6/details
 
+- NSW Bioregions Base Map - http://www.environment.gov.au/fed/catalog/search/resource/downloadData.page?uuid=%7B4A2321F0-DD57-454E-BE34-6FD4BDE64703%7D
+
 - SEPP (Koala Habitat Protection) Geo Data - https://www.planningportal.nsw.gov.au/opendata/dataset/state-environmental-planning-policy-koala-habitat-protection-2019
 
 - NSW Koala Habitat Information Base - https://datasets.seed.nsw.gov.au/dataset/koala-habitat-information-base
 
 - NSW Koala Prioritisation Project - Areas of Regional Koala Significance (ARKS) - https://datasets.seed.nsw.gov.au/dataset/areas-of-regional-koala-significance-arks
+
+- NSW Administrative Boundaries - https://portal.spatial.nsw.gov.au/portal/apps/sites/#/home/items?group_ids=2df06ccf9c5a4c869371c1a6814545b6
 
 ## Running locally
 
--- src/assets/js/mapping.js
+++ src/assets/js/mapping.js
@@ -1,34 +1,128 @@
 import * as d3 from "d3";
 import * as topojson from "topojson"
 
-
 // Svg defs
-let width = 800,
-    height = 420;
+let w = 900,
+    h = 1000;
 
 // Data defs
-let nsw = "./map-data/nsw-outline.json";
+let ausmap = "./map-data/sa4-au-geo.json";
+let towns = "./map-data/nsw-towns.json";
+let arks = "./map-data/koala-arks-geo.json";
+let extents = {
+  "au": {
+    "scale": 1050,
+    "center": [138, -23.5],
+    "bb": [[110, -6], [157, -46]]
+  },
+  "nsw": {
+    "scale": 3550,
+    "center": [148.8,-31.6],
+    "bb": [[140, -27], [154, -39]]
+  }
+};
 
-d3.json(nsw).then( (data) => {
+
+Promise.all([
+  d3.json(ausmap),
+  d3.json(towns),
+  d3.json(arks)
+]).then( (data) => drawMap(data) );
+
+
+function drawMap (data) {
+
+  let map = data[0];
+  let towns = data[1];
+  let arks = data[2];
 
   // Map defs
-  let projection = d3.geoEquirectangular().fitSize([width, height], data),
-      path = d3.geoPath().projection(projection);
+  let target = extents.nsw;
+  let projection = d3.geoMercator()
+    .center(target.center)
+    .scale(target.scale);
 
+  // Create svg canvas
   var svg = d3.select("body #nsw-map")
       .append("svg")
-      .attr("width", width)
-      .attr("height", height)
-      .attr("viewBox", [0, 0, width, height])
+      .attr("width", w)
+      .attr("height", h)
+      .attr("viewBox", [0,0,w,h])
       .attr("preserveAspectRatio", "xMinYMin");
 
+  // Define hatching pattern
+  var defs = svg.append('defs');
+  var g = defs.append("pattern")
+     .attr('id', 'hatch')
+     .attr('patternUnits', 'userSpaceOnUse')
+     .attr('width', '5')
+     .attr('height', '5')
+     .attr("x", 0).attr("y", 0)
+     .append("g")
+      .style("fill", "none")
+      .style("stroke", "white");
+    g.append("path").attr("d", "M0,5 l5,-5");
+
+  // Basemap
   svg.append('g')
     .selectAll('path')
-    .data(data.features)
+    .data(map.features)
     .join('path')
-    .attr('d', path)
+    .attr('d', d3.geoPath(projection))
     .attr('fill','none')
-    .attr('stroke', 'black');
+    .attr('stroke', (d) => d.properties.state=="NSW" ? '#fffa' : "#fff3")
+    .attr('stroke-width', '.5')
+    .attr('stroke-linejoin', 'round');
 
-    console.log(data.features);
-});
+  // ARKS
+  svg.append('g')
+    .selectAll('path')
+    .data(arks.features)
+    .join('path')
+    .attr('d', d3.geoPath(projection))
+    // .attr('fill', 'url(#hatch)')
+    .attr('fill', 'none')
+    .attr('stroke', '#0a0')
+    .attr('stroke-linejoin', 'round');
+
+  // Town dots
+  svg.append('g')
+    .selectAll('circle')
+    .data(towns.features)
+    .join('circle')
+    .attr('r', 3)
+    .attr('cx', (d) => projection(d.geometry.coordinates)[0] )
+    .attr('cy', (d) => projection(d.geometry.coordinates)[1] )
+    .attr('fill','white');
+
+  // Town names
+  svg.append('g')
+    .selectAll('text')
+    .data(towns.features)
+    .join('text')
+      .attr('fill', 'white' )
+      .attr('x', (d) => projection(d.geometry.coordinates)[0] )
+      .attr('y', (d) => projection(d.geometry.coordinates)[1] )
+      .attr('dx', 12)
+      .attr('dy', 6)
+      .attr("text-anchor", "start")
+      .text( (d) => d.properties.name );
+
+
+}
+
+
+// Helpers
+
+function toXY(lon, lat) {
+  let x = lon > 0  ? 180 + lon : -lon;
+  let y = lat < 0  ?  90 - lat : lat;
+  return [w*x/360, h*y/180];
+}
+function scaleToBB(bb) {
+  let x0 = bb[0][0], x1 = bb[1][0], y0 = bb[0][1], y1 = bb[1][1];
+  return Math.min(8, 0.9 / Math.max((x1 - x0) / w, (y1 - y0) / h));
+}
+function mean(a,b) {
+  return (a + b) / 2;
+}
