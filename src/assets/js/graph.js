aa1451f7ebc6f84a4f78c2da8b4efb65d72ffbbd
brodysmith1 <62872303+brodysmith1@users.noreply.github.com>
2020-09-13 22:23:33 +1000

    Decline extent animation

--- src/assets/js/graph.js
+++ src/assets/js/graph.js
@@ -2,72 +2,68 @@ import * as d3 from "d3";
 
 // Svg defs
 let w = 200,
-    h = 150;
+    h = 150,
+    scale = 2;
+
+w *= scale;
+h *= scale;
+
+// Anim defs
+let aoo = 0.7,
+    eoo = 0.5,
+    shrink = false;
+
+// Calculations:
+// Base size = 200 x 150 = 30K pixels
+// Base occupancy = 100 dots per 30K pixels (1 dot per 300px)
+// Scaled extent = 166 x 125 = 21K pixels (0.7 x 30K)
+// Scaled occupancy:
+//    -> 21K / 300 = 70 dots
+//    -> 0.5 x 70 dots = 35 dots
 
 // Get svg canvas
 let svg = d3.select("#graph-extent-occurence")
 let dots = svg.select('#occupancy');
+let dissolve = dots.select('.dissolve');
 let rect = svg.selectAll("#occurence");
 
-//loopD();
-//loopR();
+loopShrinkDots();
+loopShrinkRect();
+loopDissolve();
 
-function loopD() {
+function loopShrinkDots() {
+  shrink = !shrink;
   dots
     .transition()
-      .duration(100)
-      .delay(1500)
+      .delay(shrink ? 2500 : 3000)
+      .duration(1500)
+      .on('end',loopShrinkDots)
       .selectAll('circle')
-      .attr('cx', function(){ return this.getAttribute('cx')/2; } )
-      .attr('cy', function(){ return this.getAttribute('cy')/2; } );
-
-  dots
-    .transition()
-      .delay(3100)
-      .duration(100)
-      .selectAll('circle')
-      .attr('cx', function() {return this.getAttribute('cx')*2;} )
-      .attr('cy', function() {return this.getAttribute('cy')*2;} )
-    .on('end',loopD);
+      .attr('transform', function() {
+        if (!shrink) { return "translate(0 0)" }
+        else {
+          let dx = 100*(0.5-this.getAttribute('cx')*scale/w)*(1-eoo);
+          let dy = 100*(0.5-this.getAttribute('cy')*scale/h)*(1-eoo);
+          return `translate(${dx} ${dy})`;
+        }
+      });
 }
 
-function loopR() {
+function loopShrinkRect() {
   rect
     .transition()
-      .delay(1500)
+      .delay(shrink ? 2500 : 3000)
       .duration(1500)
-      .attr('transform','scale(0.7)')
-    .transition()
-      .delay(1500)
-      .duration(1500)
-      .attr('transform','scale(1)')
-    .on('end',loopR);
+      .attr('transform', shrink ? 'scale(0.833)' : 'scale(1)' )
+      .attr('stroke-width', shrink ? '1' : '0' )
+    .on('end',loopShrinkRect);
 }
 
-
-// svg.append('g')
-//   .selectAll('circle')
-//   .data(cx)
-//   .enter()
-//   .append('circle')
-//   .attr('r', 2)
-//   .attr('cx', (d,i) => cx[i] )
-//   .attr('cy', (d,i) => cy[i] );
-//
-// svg.append('g')
-//   .append('polygon')
-//   .attr('points',)
-
-// function saveSvg(svgEl, name) {
-//     svgEl.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-//     var svgData = svgEl.outerHTML;
-//     var preface = '<?xml version="1.0" standalone="no"?>\r\n';
-//     var svgBlob = new Blob([preface, svgData], {type:"image/svg+xml;charset=utf-8"});
-//     var svgUrl = URL.createObjectURL(svgBlob);
-//     var downloadLink = document.createElement("a");
-//     downloadLink.href = svgUrl;
-//     downloadLink.download = name;
-//     document.body.appendChild(downloadLink);
-//     downloadLink.click();
-//     document.body.removeChild(downloadLink);
-// } saveSvg(document.querySelector("#graph-extent-occurence svg"), 'dots');
+function loopDissolve() {
+  dissolve
+    .transition()
+      .delay(shrink ? 2500 : 3000)
+      .duration(1500)
+      .attr('opacity', () => shrink ? 0 : 1 )
+    .on('end',loopDissolve);
+}
